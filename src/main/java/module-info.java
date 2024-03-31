2c85aefb2c39cefd5794cd40853c11cfc8583e5b
Richard <125735052+RichardMartAgu@users.noreply.github.com>
2024-03-31 18:25:43 +0200

    Estructura Base

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.football_data {
+    requires javafx.controls;
+    requires javafx.fxml;
+        requires javafx.web;
+            
+            requires com.dlsc.formsfx;
+                requires org.kordamp.ikonli.javafx;
+            requires org.kordamp.bootstrapfx.core;
+            requires eu.hansolo.tilesfx;
+        
+    opens com.example.football_data to javafx.fxml;
+    exports com.example.football_data;
+}
